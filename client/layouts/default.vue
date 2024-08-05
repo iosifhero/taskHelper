6568a30632044fa1f652b6e0f13288d74da84381
Iosif Baburov <baburov.iosif@yandex.ru>
2024-08-06 01:16:16 +0700

    add: draft structure

---
+++ client/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-container>
+        <el-header>
+                <div class="mb-4">
+                    <el-button :type="''" link >Декомпозиция</el-button>
+                    <el-button :type="''" link >Канбан</el-button>
+                </div>
+                <div>
+                    <el-avatar :icon="UserFilled" />
+                </div>
+        </el-header>
+    <slot></slot>
+</el-container>
+</template>
+
+<script setup lang="ts">
+import {UserFilled} from "@element-plus/icons-vue";
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <el-container>
+        <el-aside>
+            <el-card style="max-width: 480px">
+                <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+            </el-card>
+        </el-aside>
+        <el-main>main</el-main>
+    </el-container>
+</template>
+
